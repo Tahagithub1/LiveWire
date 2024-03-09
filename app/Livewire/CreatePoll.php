33733f1fc281d11121e1bbba4059f1789dadc242
Tahagithub1 <mogevaraluffyy@gmail.com>
2024-03-09 07:55:24 +0330

    Add-Remove-Option

--- app/Livewire/CreatePoll.php
+++ app/Livewire/CreatePoll.php
@@ -8,8 +8,22 @@ class CreatePoll extends Component
 {
     public $title;
 
+     public $options = ['first'];
+
     public function render()
     {
         return view('livewire.create-poll');
+    }
+    public function AddOption()  {
+
+    $this->options[] = '';
+
+    }
+
+    public function RemoveOption($index)  {
+
+     unset($this->options[$index]);
+      $this->options = array_values($this->options);
+
     }
 }
--- resources/views/livewire/create-poll.blade.php
+++ resources/views/livewire/create-poll.blade.php
@@ -3,5 +3,22 @@
     <label> poll title </label>
     <input type="text" wire:model.live="title" />
     this : {{ $title }}
+    <div class="mb-4 mt-4">
+         <button class="btn" wire:click.prevent="AddOption"> add option </button>
+    </div>
+    @foreach ($options as $index => $option)
+    <div class="mb-4">
+        <label> option {{ $index + 1 }} </label>
+        <div class="mb-4" >
+            <input type="text" wire:model="options.{{ $index }}">
+            <div class="mb-4">
+                <button class="btn" wire:click.prevent="RemoveOption({{ $index }})"  > Remove </button>
+            </div>
+            {{-- {{ $index }} - {{ $option }} --}}
+        </div>
+
+    </div>
+    @endforeach
+
    </form>
 </div>
